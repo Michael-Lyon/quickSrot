05259d249ce3645e7e89e44ca5893d9d683e9588
Michael Lyon <michaellyon@localhost.localdomain>
2023-02-21 08:30:36 +0100

    ready6

--- quickSort/urls.py
+++ quickSort/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 from core import views
 
@@ -24,4 +27,11 @@ router.register(f'details', views.DetailViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
-]
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
