db353d6371d4685af2534fc57d3de6906e0d4893
Michael Lyon <michaellyon@localhost.localdomain>
2023-02-21 08:35:16 +0100

    ready7

--- quickSort/urls.py
+++ quickSort/urls.py
@@ -35,3 +35,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+
+CSRF_TRUSTED_ORIGINS = ['https://fuck-u-celestine.up.railway.app']
